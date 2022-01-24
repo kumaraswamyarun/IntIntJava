a59d97060781a301728bc098f7a82bc290b0c621
simon <simon@dancingcloudservices.com>
2022-01-24 13:03:48 -0700

    Day 1 lunch

--- src/main/java/students/Student.java
+++ src/main/java/students/Student.java
@@ -1,12 +1,36 @@
 package students;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
-public class Student {
+// Java has 8 builtin primitive types:
+// boolean, byte, short, char, int, long, float, double
+// EVERYTHING ELSE is a "reference type" (i.e. an object)
+// they ALWAYS (unless someone massively breaks the conventions)
+// start with an upper case letter.
+
+// immutability can be bypassed if you have non-final classes!!!
+public final class Student {
   private final String name;
   private final double gpa;
   private final List<String> courses;
 
+  // if caller calls with comma separated Strings, compiler builds an array
+  // and this does not leak.
+  // BUT call could call this with an actual array!!!!
+  public Student(String name, double gpa, String ... courses) {
+    List<String> courseNames = Arrays.asList(courses);
+    if (! isValid(name, gpa, courseNames)){
+      throw new IllegalArgumentException("Bad values for a Student");
+    }
+    this.name = name;
+    this.gpa = gpa;
+    this.courses = courseNames;
+  }
+
+  // never use directly caller-provided mutable data in our own structure
+  // if we want to be immutable!
   public Student(String name, double gpa, List<String> courses) {
     if (! isValid(name, gpa, courses)){
       throw new IllegalArgumentException("Bad values for a Student");
@@ -25,11 +49,12 @@ public class Student {
   }
 
   public List<String> getCourses() {
-    return courses;
-  }
+    // return a defensive copy?
+    // is there an immutable list
 
-  // Fix Mina's IDE
-  // Make this immutable again :)
+    // return a proxy that rejects changes
+    return Collections.unmodifiableList(courses);
+  }
 
   public Student withName(String newName) {
     if (!isValid(newName, this.gpa, this.courses)) {
